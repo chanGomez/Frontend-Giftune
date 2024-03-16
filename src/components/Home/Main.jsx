821f61fd8fe774626f1690059bf4273ac79f77b4
Chantal <chantalgomez@pursuit.org>
2024-03-15 20:53:12 -0400

    Main view of markting page is set. Media queries not yet there.

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -1,40 +1,44 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import Chip from '@mui/material/Chip';
-import Container from '@mui/material/Container';
+import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
-import DevicesRoundedIcon from '@mui/icons-material/DevicesRounded';
-import EdgesensorHighRoundedIcon from '@mui/icons-material/EdgesensorHighRounded';
-import ViewQuiltRoundedIcon from '@mui/icons-material/ViewQuiltRounded';
+import Box from '@mui/material/Box';
+import { Container } from '@mui/material';
+import marketingImage from '../../Assets/marketing-image.png'
+import Button from '@mui/material/Button';
+
 
 function Main() {
 
   return (
-    <Container id="features" sx={{ py: { xs: 8, sm: 16 } }}>
-    <Box
-      height={200}
-      width={800}
-      my={4}
-      display="flex"
-      alignItems="center"
-      gap={4}
-      p={2}
-    //   sx={{ border: '2px solid grey' }}
-    >
-    <Typography variant="h1" gutterBottom sx={{fontWeight: 500}}>
-        Give what they love to who you love.
-      </Typography>
-    </Box>
-        <Box>
-
-        </Box>
+    <Container sx={{ py: 15 }} >
+    <Grid container>
+        <Grid
+          xs={6}
+          sx={{ py: 10 }}
+        >
+          <Typography component="h1" variant="h2" color="inherit" gutterBottom>
+            Gift what they love to who you love.
+          </Typography>
+          <Typography variant="h6" color="inherit" paragraph>
+          An app designed to keep you on top of your loved ones upcoming
+          birthdays, where you can effortlessly select the perfect gift.
+          </Typography>
+          <Link variant="subtitle1" href="#">
+          <Button variant="contained" color="primary" sx={{ flexShrink: 0 }}>
+                SIGN UP
+              </Button>
+          </Link>
+        </Grid>
+        <Grid xs={6}>
+        <img src={marketingImage} alt={"marketingImage"}  style={{ width: 600}}/>
+        </Grid>
+    </Grid>
     </Container>
+
+
   )
 }
 
